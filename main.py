831a460d6615acf18f4612d92248c2c2d9bc0596
HarshitSoni <soni.harshit.1903@gmail.com>
2017-11-15 17:42:47 +0530

    V0
    repository created

---
+++ main.py
@@ -0,0 +1,7 @@
+"""The following code is explicitly for educational purpose only. The logic behind the program belongs to Mr. Mohit, a penetration tester.
+This code has been provided as is. and I shall not be hold responsible for any damage caused by this code, legal or not. The user is
+solely responsible for his doings. You may use this code only if you agree to abide by the above mentioned clause. If not you should
+destroy this file at once. This code can seriously cause damage to systems, thus causing severe capital loss, and defamation."""
+
+def dos():
+    
